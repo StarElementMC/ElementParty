a5817227c601bfef517350b3d16c319639f0cb45
Ruokwok <i@ruok.cc>
2025-09-25 22:53:20 +0800

    玩家加入时传送至世界出生点

--- src/main/java/net/starelement/BaseListener.java
+++ src/main/java/net/starelement/BaseListener.java
@@ -1,9 +1,19 @@
 package net.starelement;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.level.Position;
 
 public class BaseListener implements Listener {
 
-
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Position position = Server.getInstance().getDefaultLevel().getSpawnLocation();
+        player.teleport(position);
+    }
 
 }
