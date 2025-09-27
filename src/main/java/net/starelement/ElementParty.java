35236925080a87ad7dd483e3853209b1ae6c1a9c
Ruokwok <i@ruok.cc>
2025-09-27 17:22:01 +0800

    清理残留地图

--- src/main/java/net/starelement/ElementParty.java
+++ src/main/java/net/starelement/ElementParty.java
@@ -1,17 +1,22 @@
 package net.starelement;
 
+import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
 import net.starelement.cmds.PartyCommand;
 import net.starelement.game.GameManager;
 import net.starelement.game.LevelTemplate;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 public class ElementParty extends PluginBase {
 
     @Override
     public void onLoad() {
         GameManager.getInstance().plugin = this;
+        clear();
         File lt = new File(LevelTemplate.TEMPLATE_PATH);
         if (!lt.exists()) lt.mkdir();
         getServer().getCommandMap().register("element", new PartyCommand("ep"));
@@ -25,5 +30,19 @@ public class ElementParty extends PluginBase {
     @Override
     public void onDisable() {
         getServer().getCommandMap().unregister("element");
+    }
+
+    private void clear() {
+        Object world = getServer().getProperty("level-name");
+        File worlds = new File("worlds/");
+        for (File file : Objects.requireNonNull(worlds.listFiles())) {
+            if (!file.getName().equals(world)) {
+                try {
+                    FileUtils.deleteDirectory(file);
+                } catch (IOException e) {
+                    getServer().getLogger().logException(e);
+                }
+            }
+        }
     }
 }
