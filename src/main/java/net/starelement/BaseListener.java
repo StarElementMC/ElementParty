7db711bc94e50c561eeaf18af27aa3f6258ad1f6
Ruokwok <i@ruok.cc>
2025-09-28 14:14:23 +0800

    玩家不足时结束游戏

--- src/main/java/net/starelement/BaseListener.java
+++ src/main/java/net/starelement/BaseListener.java
@@ -5,7 +5,10 @@ import cn.nukkit.Server;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
 import cn.nukkit.level.Position;
+import net.starelement.game.GameManager;
+import net.starelement.game.Party;
 
 public class BaseListener implements Listener {
 
@@ -14,6 +17,17 @@ public class BaseListener implements Listener {
         Player player = event.getPlayer();
         Position position = Server.getInstance().getDefaultLevel().getSpawnLocation();
         player.teleport(position);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        Party party = GameManager.getInstance().getParty();
+        if (party != null) {
+            if (Server.getInstance().getOnlinePlayers().values().size() <= 1) {
+                party.finish();
+            }
+        }
     }
 
 }
