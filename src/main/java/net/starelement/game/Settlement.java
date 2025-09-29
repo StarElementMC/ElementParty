ccd5cd61045bbc7e2d4e2cf990bd3a9b4a8a3d1b
Ruokwok <i@ruok.cc>
2025-09-29 14:16:03 +0800

    添加结算数据

---
+++ src/main/java/net/starelement/game/Settlement.java
@@ -0,0 +1,15 @@
+package net.starelement.game;
+
+import net.starelement.game.set.PlayerSet;
+
+import java.util.HashMap;
+
+public class Settlement {
+
+    public PlayerSet qualifiedPlayers;
+
+    public PlayerSet eliminatedPlayers;
+
+    public HashMap<PartyPlayer, Integer> scoreMap;
+
+}
